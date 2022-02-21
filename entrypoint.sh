b1d78a0b97336bd97296b844bd5e437a5a7fa22d
kb2099 <62949405+kb2099@users.noreply.github.com>
2022-02-21 09:17:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
-
-# Global variables
-DIR_CONFIG="/etc/v2ray"
-DIR_RUNTIME="/usr/bin"
-DIR_TMP="$(mktemp -d)"
-
+# V2Ray new configuration
+# Run V2ray
 # Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+cat <<-EOF > $ /etc/v2ray/config.json
 {
     "inbounds": [{
-        "port": ${PORT},
+        "port": 80,
         "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "${ID}",
+                "id": "ba651c8c-cb35-4c46-bf6c-f90bd6f094e3",
                 "alterId": 0
             }]
         },
         "streamSettings": {
             "network": "ws",
             "wsSettings": {
-                "path": "${WSPATH}"
+                "path": "/laowang"
             }
         }
     }],
